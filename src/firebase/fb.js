80ee01b51d93c26b4998b27b522427fffedcae07
JulianHerreraH <42717697+JulianHerreraH@users.noreply.github.com>
2020-09-05 14:05:59 -0600

    Removed Firebase credentials

--- src/firebase/fb.js
+++ src/firebase/fb.js
@@ -5,13 +5,7 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAWF2P-uzKBgHcqrzc9-gZctR1hMfpvcCc",
-    authDomain: "food-blog-da3f9.firebaseapp.com",
-    databaseURL: "https://food-blog-da3f9.firebaseio.com",
-    projectId: "food-blog-da3f9",
-    storageBucket: "food-blog-da3f9.appspot.com",
-    messagingSenderId: "591390816694",
-    appId: "1:591390816694:web:6eb1c3de9ae76ee6f5a917"
+    // Credentials
 }
 
 // Initialize Firebase
